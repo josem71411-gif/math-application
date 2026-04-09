00956d316d19855bad6d3244884f980bb74f6e91
josem71411-gif <josem71411@gmail.com>
2026-04-09 15:14:30 -0500

    Question 1

--- src/main/java/com/plurasight/mathApp.java
+++ src/main/java/com/plurasight/mathApp.java
@@ -1,4 +1,21 @@
 package com.plurasight;
 
 public class mathApp {
+    public static void main(String[] args) {
+// Question 1
+        double bobSalary = 55000;
+        double garySalary = 62000;
+        double highestSalary;
+        highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is "+ highestSalary);
+
+
+
+
+
+
+
+
+
+    }
 }
